PromyLOPh/crocoite

commit a21d7332e33a3e47a363004196451721d449e70b

Move getResponseBody call to Item wrapper

crocoite/browser.py
@@ -24,13 +24,15 @@ Chrome browser interactions.
 
 import logging
 from urllib.parse import urlsplit
+from base64 import b64decode
 
 class Item:
     """
     Simple wrapper containing Chrome request and response
     """
 
-    def __init__ (self):
+    def __init__ (self, tab):
+        self.tab = tab
         self.chromeRequest = None
         self.chromeResponse = None
         self.chromeFinished = None
@@ -57,6 +59,21 @@ class Item:
     @property
     def encodedDataLength (self):
         return self.chromeFinished['encodedDataLength']
+
+    @property
+    def body (self):
+        """ Return response body or None """
+        try:
+            body = self.tab.Network.getResponseBody (requestId=self.id)
+            rawBody = body['body']
+            base64Encoded = body['base64Encoded']
+            if base64Encoded:
+                rawBody = b64decode (rawBody)
+            else:
+                rawBody = rawBody.encode ('utf8')
+            return rawBody
+        except pychrome.exceptions.CallMethodException:
+            return None
 
     def setRequest (self, req):
         self.chromeRequest = req
@@ -185,7 +202,7 @@ class SiteLoader:
             else:
                 self.logger.warn ('request {} already exists, overwriting.'.format (reqId))
 
-        item = Item ()
+        item = Item (self.tab)
         item.setRequest (kwargs)
         self.requests[reqId] = item
 

crocoite/warc.py
@@ -25,12 +25,10 @@ Classes writing data to WARC files
 import logging
 import json
 from http.server import BaseHTTPRequestHandler
-from base64 import b64decode
 from io import BytesIO
 from warcio.statusandheaders import StatusAndHeaders
 from urllib.parse import urlsplit
 from logging.handlers import BufferingHandler
-import pychrome
 from datetime import datetime
 from threading import Thread
 from queue import Queue
@@ -188,15 +186,8 @@ class WarcLoader (AccountingSiteLoader):
             raise ValueError ('body for {} too large {} vs {}'.format (reqId,
                     item.encodedDataLength, self.maxBodySize))
         else:
-            try:
-                body = self.tab.Network.getResponseBody (requestId=reqId)
-                rawBody = body['body']
-                base64Encoded = body['base64Encoded']
-                if base64Encoded:
-                    rawBody = b64decode (rawBody)
-                else:
-                    rawBody = rawBody.encode ('utf8')
-            except pychrome.exceptions.CallMethodException:
+            rawBody = item.body
+            if rawBody is None:
                 raise ValueError ('no data for {} {} {}'.format (resp['url'],
                     resp['status'], reqId))
         return rawBody, base64Encoded

commit 4a6494b19f287848588641aa1330807e69031e8b

Fix base64 body detection
Broken by commit a21d7332e33a3e47a363004196451721d449e70b

crocoite/browser.py
@@ -76,9 +76,9 @@ class Item:
                 rawBody = b64decode (rawBody)
             else:
                 rawBody = rawBody.encode ('utf8')
-            return rawBody
+            return rawBody, base64Encoded
         except (pychrome.exceptions.CallMethodException, pychrome.exceptions.TimeoutException):
-            return None
+            return None, False
 
     def setRequest (self, req):
         self.chromeRequest = req
@@ -484,7 +484,7 @@ class TestSiteLoader (unittest.TestCase):
             for item in l.finished:
                 if item.url.endswith ('/empty'):
                     self.assertEqual (item.response['status'], 200)
-                    self.assertEqual (item.body, b'')
+                    self.assertEqual (item.body[0], b'')
                 elif item.url.endswith ('/redirect/301'):
                     self.assertEqual (item.response['status'], 301)
                 else:
@@ -499,7 +499,7 @@ class TestSiteLoader (unittest.TestCase):
                 l.waitIdle ()
                 self.assertEqual (len (l.finished), 1)
                 self.assertUrls (l, ['/encoding/{}'.format (encoding)])
-                self.assertEqual (l.finished[0].body, expected.encode ('utf8'))
+                self.assertEqual (l.finished[0].body[0], expected.encode ('utf8'))
 
     def test_binary (self):
         """ Browser should ignore content it cannot display (i.e. octet-stream) """
@@ -515,7 +515,7 @@ class TestSiteLoader (unittest.TestCase):
             l.waitIdle ()
             self.assertEqual (len (l.finished), 1)
             self.assertUrls (l, ['/image'])
-            self.assertEqual (l.finished[0].body, TestHTTPRequestHandler.imageTestData)
+            self.assertEqual (l.finished[0].body[0], TestHTTPRequestHandler.imageTestData)
 
     def test_attachment (self):
         """ And downloads won’t work in headless mode """
@@ -533,10 +533,10 @@ class TestSiteLoader (unittest.TestCase):
             for item in l.finished:
                 if item.url.endswith ('/html'):
                     self.assertEqual (item.response['status'], 200)
-                    self.assertEqual (item.body, TestHTTPRequestHandler.htmlTestData.encode ('utf-8'))
+                    self.assertEqual (item.body[0], TestHTTPRequestHandler.htmlTestData.encode ('utf-8'))
                 elif item.url.endswith ('/image'):
                     self.assertEqual (item.response['status'], 200)
-                    self.assertEqual (item.body, TestHTTPRequestHandler.imageTestData)
+                    self.assertEqual (item.body[0], TestHTTPRequestHandler.imageTestData)
                 elif item.url.endswith ('/nonexistent'):
                     self.assertEqual (item.response['status'], 404)
                 else:
@@ -550,10 +550,10 @@ class TestSiteLoader (unittest.TestCase):
             for item in l.finished:
                 if item.url.endswith ('/alert'):
                     self.assertEqual (item.response['status'], 200)
-                    self.assertEqual (item.body, TestHTTPRequestHandler.alertData.encode ('utf-8'))
+                    self.assertEqual (item.body[0], TestHTTPRequestHandler.alertData.encode ('utf-8'))
                 elif item.url.endswith ('/image'):
                     self.assertEqual (item.response['status'], 200)
-                    self.assertEqual (item.body, TestHTTPRequestHandler.imageTestData)
+                    self.assertEqual (item.body[0], TestHTTPRequestHandler.imageTestData)
                 else:
                     self.fail ('unknown url')
 

crocoite/warc.py
@@ -186,7 +186,7 @@ class WarcLoader (AccountingSiteLoader):
             raise ValueError ('body for {} too large {} vs {}'.format (reqId,
                     item.encodedDataLength, self.maxBodySize))
         else:
-            rawBody = item.body
+            rawBody, base64Encoded = item.body
             if rawBody is None:
                 raise ValueError ('no data for {} {} {}'.format (resp['url'],
                     resp['status'], reqId))